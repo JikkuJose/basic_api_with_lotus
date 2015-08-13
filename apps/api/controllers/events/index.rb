1ea15a5efddcb3cfd824c0d1c24bbfb30e9a21cd
Jikku Jose <jikkujose@gmail.com>
2015-08-13 11:05:53 +0530

    Update controller for json

--- apps/api/controllers/events/index.rb
+++ apps/api/controllers/events/index.rb
@@ -1,7 +1,27 @@
+require 'json'
+
 module API::Controllers::Events
   class Index
+    include API::Action
+
     def call(env)
-      [200, {}, ['from Controller!']]
+      self.format = :json
+
+      self.body = sample_hash.to_json
+    end
+
+    def sample_hash
+      {
+        error: nil,
+        payload: [
+          {
+            id: 1,
+            type: :accident,
+            lattitude: 1.9,
+            longitude: 2.3,
+          }
+        ],
+      }
     end
   end
 end
